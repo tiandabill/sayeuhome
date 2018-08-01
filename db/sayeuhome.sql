3977b9327e2cb8fe883149e107ab5d8a13e97a51
Bill Liu <tiandabill@msn.com>
2018-08-01 23:56:17 +0200

    add database sql file and java for controller

--- db/sayeuhome.sql
+++ db/sayeuhome.sql
@@ -24,5 +24,24 @@ insert into articles values (
 
 select * from articles;
 
+insert into articles values (
+2,
+'second one',
+'Bill Liu',
+'Hello World again',
+'2018-08-01 09:32',
+'2018-08-01 09:33',
+'article, world, post');
+
+create table users (
+    user_id int primary key auto_increment,
+    name varchar(50),
+    pwd varchar(20),
+    email varchar(100),
+    create_date datetime,
+    birth_date date,
+    statue varchar(20),
+    tag varchar(100));
+
 -- drop table articles;
     
